13ad699b7bc3debe244ad98512fb0c612c37ef6b
ksw15 <35079103+ksw15@users.noreply.github.com>
2018-01-17 23:52:17 +0100

    Add files via upload
    dodanie dodatkowej funnkcjonalnosci aplikacji

--- src/main/java/controllers/AuthorizationFailedController.java
+++ src/main/java/controllers/AuthorizationFailedController.java
@@ -1,5 +1,30 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class AuthorizationFailedController {
+	
+	 @FXML
+	    private StackPane AuthorizationFailedZamknijButton;
+
+	    @FXML
+	    void OnClickAuthorizationFailedZamknijButton(MouseEvent event) 
+	    {
+	    	/*try
+	    	{
+	    		
+	    	Stage stage = (Stage) AuthorizationFailedZamknijButton.getScene().getWindow();
+		    stage.close();	
+		    
+	    	}catch (Exception e) {
+	    			System.out.println("Nie mozna otworzyc nowego okna");
+	    	}*/
+	    }
+	    
+	    
+	    
 
 }
